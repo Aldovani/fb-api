0d1c93e1ea70e296e8e4a28e67fa35ed27ab7bef
Aldovani <aldovanihcosta@gmail.com>
2022-05-01 22:37:16 -0300

    Update

--- app/Utils/index.ts
+++ app/Utils/index.ts
@@ -1,2 +1,6 @@
 export * from './fileCategories'
 export * from './reactionsTypes'
+export * from './isFollowing'
+export * from './OnlineUser'
+export * from './updateOnlineFollowing'
+export * from './getIds'
